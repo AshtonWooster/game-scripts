95644565ee09b78c4d6d1b3bd45de15a53753c0d
Ashton <woosterashton48@gmail.com>
2023-10-02 22:05:49 -0500

    Shows char icon
    Shows char icons in the char select

--- skill-tree-game/StarterPlayer/StarterCharacterScripts/CharSelect.lua
+++ skill-tree-game/StarterPlayer/StarterCharacterScripts/CharSelect.lua
@@ -11,12 +11,39 @@ local charSelectFolder = gui:WaitForChild("CharacterSelect")
 local charMenu = charSelectFolder:WaitForChild("SelectMenu")
 local charMove = charMenu:WaitForChild("Move")
 local charClose = charMenu:WaitForChild("Close")
+local scrollFrame = charMenu:WaitForChild("Frame")
+local templateImage = scrollFrame:WaitForChild("Template")
 local gameState = player:WaitForChild("GameState")
 local charOpen = charSelectFolder:WaitForChild("CharOpen")
+local classesFolder = repStorage:WaitForChild("Classes")
 
 --Modules--
 local guiManip = require(clientMods:WaitForChild("GuiManip"))
 local mouseController = require(clientMods:WaitForChild("Mouse"))
+
+--Constants--
+local NUM_CHAR_IN_ROW = 5
+
+--Place all char Images--
+local templateSize, xOffset, yOffset, offset do
+	templateSize = templateImage.Size
+	offset = (1 - templateSize.X.Scale * NUM_CHAR_IN_ROW) / (NUM_CHAR_IN_ROW + 1)
+	xOffset = templateSize.X.Scale + offset
+	yOffset = templateSize.Y.Scale + offset
+	
+	for i, char in pairs(classesFolder:GetChildren()) do
+		local configFolder = char.Config
+		local image = configFolder.Image.Value
+		local newImage = templateImage:Clone()
+		
+		newImage.Parent = scrollFrame
+		newImage.Image = image
+		newImage.Name = tostring(i)
+		newImage.Visible = true
+		
+		newImage.Position = UDim2.fromScale(xOffset * (i % (NUM_CHAR_IN_ROW + 1)), yOffset * math.ceil(i / NUM_CHAR_IN_ROW) - offset/2)
+	end
+end
 
 --Hide/Show Char Select Menu--
 gameState.Changed:Connect(function(value)
